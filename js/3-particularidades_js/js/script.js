1bbeebd5b53eac7afb07ab6c9db2bf27f33b8ddb
Fabricio s Andrade <143853928+nocreati@users.noreply.github.com>
2023-12-11 22:35:05 -0300

    Add files via upload

---
+++ js/3-particularidades_js/js/script.js
@@ -0,0 +1,18 @@
+console.log("primeiro")
+console.log("segundo")
+console.log("terceiro")
+
+//exemplo de comentario
+
+var Nomecliente = 2;
+let nomeUsuario = 3;
+const b = 4;
+
+/**let me go way let me going up  */
+
+Nomecliente = 'texto';
+
+
+
+console.log(Nomecliente);
+console.log(b);
---
+++ js/5-numeros/js/script.js
@@ -0,0 +1,12 @@
+var numero = 5;
+console.log(numero);
+
+console.log(typeof(numero));
+
+var float = 5.20;
+console.log(float);
+console.log(typeof(float));
+
+var textNumber = '523';
+console.log(textNumber);
+console.log(typeof(textNumber));
